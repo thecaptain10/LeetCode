9ff1596ca5786fcc749f34ab4f4127d902f70bb9
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-06-19 21:45:40 +0530

    Remove Duplicates From Sorted List II

---
+++ LeetCode/LeetCodeProblems/Medium/RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    public class RemoveDuplicatesFromSortedListII
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            ListNode fakeHead = new ListNode(0);
+            fakeHead.next = head;
+
+            ListNode prev = fakeHead;
+            while (prev.next != null && prev.next.next != null)
+            {
+                if (prev.next.val == prev.next.next.val)
+                {
+                    int dup = prev.next.val;
+                    while (prev.next != null && prev.next.val == dup)
+                    {
+                        prev.next = prev.next.next;
+                    }
+                }
+                else
+                {
+                    prev = prev.next;
+                }
+
+            }
+
+            return fakeHead.next;
+        }
+    }
+}
