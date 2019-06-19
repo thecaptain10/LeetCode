4efc0df8af5bfb652199b776f39cddfe718e2a59
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-06-19 21:15:23 +0530

    Linked List Related Problems

---
+++ LeetCode/LeetCodeProblems/Easy/LinkedListCycle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/linked-list-cycle/
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class LinkedListCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null)
+                return false;
+
+            if (head.next == null)
+                return false;
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            // If there is a cycle, fast and slow pointers will be equal at some point of time for sure.
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeProblems/Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+   
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+
+            ListNode fakeHead = new ListNode(0);
+
+            ListNode p1 = l1;
+            ListNode p2 = l2;
+            ListNode p3 = fakeHead;
+
+            while (p1 != null && p2 != null)
+            {
+                if (p1.val < p2.val)
+                {
+                    p3.next = p1;
+                    p1 = p1.next;
+                }
+                else
+                {
+                    p3.next = p2;
+                    p2 = p2.next;
+                }
+
+                p3 = p3.next;
+            }
+            if (p1 != null)
+            {
+                p3.next = p1;
+            }
+            if (p2 != null)
+            {
+                p3.next = p2;
+            }
+            
+            return fakeHead.next;
+        }
+    }
+}
