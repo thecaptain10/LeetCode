7ea92062f964537efad8acc84d90c1f431a070ec
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-06-21 19:51:12 +0530

    Valid BST

---
+++ LeetCode/LeetCodeProblems/Medium/ValidateBST.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    public class ValidateBST
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return Validate(root, double.NegativeInfinity, double.PositiveInfinity);
+        }
+        private bool Validate(TreeNode root, double minValue, double maxValue)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            if (root.val >= maxValue || root.val <= minValue)
+            {
+                return false;
+            }
+            bool isLeftTreeValid = Validate(root.left, minValue, root.val);
+            bool isRightTreeValid = Validate(root.right, root.val, maxValue);
+            return isLeftTreeValid && isRightTreeValid;
+        }
+    }
+}
