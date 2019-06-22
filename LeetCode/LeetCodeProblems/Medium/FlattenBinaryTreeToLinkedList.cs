0008e7d11ac4c7f92752206c51a9a25499fb2ebc
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-06-22 12:43:10 +0530

    FlattenBinaryTreeToLinkedList

---
+++ LeetCode/LeetCodeProblems/Medium/FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    //https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+    //Given a binary tree, flatten it to a linked list in-place.
+    public class FlattenBinaryTreeToLinkedList
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null) return;
+            var left = root.left;
+            var right = root.right;
+
+            root.left = null;
+            Flatten(left);
+            Flatten(right);
+
+            root.right = left;
+            TreeNode current = root;
+            while (current.right != null) current = current.right;
+
+            current.right = right;
+        }
+    }
+}
