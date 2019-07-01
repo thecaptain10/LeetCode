5a126f57d4c0b1fd5b9371c7aa635e886b97f157
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-07-02 00:25:25 +0530

    Distinct subseq comments

--- LeetCode/LeetCodeProblems/Hard/DistinctSubsequences.cs
+++ LeetCode/LeetCodeProblems/Hard/DistinctSubsequences.cs
@@ -21,6 +21,9 @@ namespace LeetCodeProblems.Hard
                 dp[i, 0] = 1;
             }
 
+            //C[i][j] = C[i-1][j]. No matter what current char of S is we simply don't use it. We will only use chars [0,...i-2] from S no matter how many solutions there are to cover T[0...j-1]
+            // But if current char of S is same to current of T (S[i - 1] == T[j - 1]) then we have another choice: we can use all the solutions of C[i - 1][j - 1] to increment the solution C[i][j].Therefore C[i][j] += C[i - 1][j - 1]
+
             for (int i = 1; i <= m; i++)
             {
                 for (int j = 1; j <= n; j++)
