a012754a2038ee24b7159befdc762d674d699a7c
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-07-10 10:17:46 +0530

    Lowest common ancestor In BST

---
+++ LeetCode/LeetCodeProblems/Easy/LowestCommonAncestorInBST.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+    //Lowest Common Ancestor of a Binary Search Tree
+    public class LowestCommonAncestorInBST
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null)
+                return null;
+            //if both values are smaller, check in left subtree.
+            //if both values are greater, check in right subtree
+            //else return root.
+            if (root.val < p.val && root.val < q.val)
+                return LowestCommonAncestor(root.right, p, q);
+            else if (root.val > p.val && root.val > q.val)
+                return LowestCommonAncestor(root.left, p, q);
+
+            return root;
+        }
+    }
+}
