7120f46334ad0817f89e346bdfe26d56d515c12f
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-07-23 10:59:44 +0530

    Min depth of Tree

---
+++ LeetCode/LeetCodeProblems/Easy/MinDepthOfTree.cs
@@ -0,0 +1,41 @@
+﻿using LeetCodeProblems.Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/minimum-depth-of-binary-tree/
+    //Minimum Depth of Binary Tree
+    public class MinDepthOfTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            //Leaf Node
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+
+            // If left subtree is NULL, recur for right subtree  
+            if (root.left == null)
+            {
+                return MinDepth(root.right) + 1;
+            }
+
+            // If right subtree is NULL, recur for left subtree  
+            if (root.right == null)
+            {
+                return MinDepth(root.left) + 1;
+            }
+
+            return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+
+        }
+    }
+}
