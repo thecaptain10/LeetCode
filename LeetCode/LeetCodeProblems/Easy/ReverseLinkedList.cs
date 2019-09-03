cbc706814145b957522389e48ff21f55e5b24b3d
Purnimesh Srivastava <pusrivas@microsoft.com>
2019-09-03 19:40:34 +0530

    Reverse Linked List

---
+++ LeetCode/LeetCodeProblems/Easy/ReverseLinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/reverse-linked-list/
+    //Reverse Linked List
+    //O(n)
+    public class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null)
+            {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+    }
+}
