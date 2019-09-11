9dfeaf4a556db599e4cac0ba215e605819b7be82
Purnimesh Srivastava <purnimesh@gmail.com>
2019-09-11 09:03:15 +0530

    Reorder Data In Log Files

---
+++ LeetCode/LeetCodeProblems/Easy/ReorderDataInLogFiles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/reorder-data-in-log-files/
+    //Reorder data in log files.
+    public class ReorderDataInLogFiles
+    {
+        public string[] ReorderLogFiles(string[] logs)
+        {
+
+            List<string> letterLogs = new List<string>();
+            List<string> digitLogs = new List<string>();
+
+            foreach (var log in logs)
+            {
+
+                if (Char.IsDigit(log.Substring(log.IndexOf(" ")).Replace(" ", string.Empty)[0]))
+                {
+                    digitLogs.Add(log);
+                }
+                else
+                {
+                    letterLogs.Add(log);
+                }
+            }
+
+
+            var letterLogsSorted = letterLogs.OrderBy(l => l.Substring(l.IndexOf(" "))).ThenBy(l => l.Split(' ')[0]).ToList();
+
+            letterLogsSorted.AddRange(digitLogs.ToList());
+
+            return letterLogsSorted.ToArray();
+
+        }
+    }
+}
