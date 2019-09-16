965618c0ba77e39f33c2c26371e0708572bc632b
Purnimesh Srivastava <purnimesh@gmail.com>
2019-09-16 10:45:06 +0530

    Valid Sudoku

---
+++ LeetCode/LeetCodeProblems/Medium/ValidSudoku.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    //https://leetcode.com/problems/valid-sudoku/
+    //Determine if a 9x9 Sudoku board is valid. 
+    public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                var hashSetRow = new HashSet<char>();
+
+                var hashSetColumn = new HashSet<char>();
+
+                var hashSetGrid = new HashSet<char>();
+
+                for (var j = 0; j < 9; j++)
+                {
+                    if (board[i][j] != '.' && !hashSetRow.Add(board[i][j])) return false;
+
+                    if (board[j][i] != '.' && !hashSetColumn.Add(board[j][i])) return false;
+
+                    var x = (i % 3) * 3 + j % 3;
+
+                    var y = (i / 3) * 3 + j / 3;
+
+                    if (board[x][y] != '.' && !hashSetGrid.Add(board[x][y])) return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
