fa4a133a5092991d0042d42302da1b89102fa4c9
Purnimesh Srivastava <purnimesh@gmail.com>
2019-10-07 10:01:45 +0530

    Plus One

---
+++ LeetCode/LeetCodeProblems/Easy/PlusOneToIntInArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/plus-one/
+    //Given a non-empty array of digits representing a non-negative integer, plus one to the integer.The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+    public class PlusOneToIntInArray
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            List<int> result = new List<int>();
+
+            int carry = 0;
+            int n = digits.Length;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int oneResult = digits[i] + carry;
+                if (i == n - 1)
+                {
+                    oneResult += 1;
+                }
+                carry = oneResult / 10;
+                result.Add(oneResult % 10);
+            }
+
+            if (carry != 0)
+            {
+                result.Add(carry);
+            }
+
+            result.Reverse();
+
+            return result.ToArray();
+        }
+    }
+}
