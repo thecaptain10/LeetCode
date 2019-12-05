1563a8c492cabb36837bd0e54e197f22b71ebefe
Purnimesh Srivastava <purnimesh@gmail.com>
2019-12-05 21:58:12 +0530

    Best Time to Buy and Sell Stock II

---
+++ LeetCode/LeetCodeProblems/Easy/BestTimeToBuySellStockII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Easy
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+    //Say you have an array for which the ith element is the price of a given stock on day i.
+    //Design an algorithm to find the maximum profit.You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+    //O(n)
+    public class BestTimeToBuySellStockII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0)
+                return 0;
+
+            int profit = 0;
+            //Greedy : If next day price is more sell
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i + 1] > prices[i])
+                    profit += prices[i + 1] - prices[i];
+            }
+            return profit;
+        }
+    }
+}
