0f704c9a187b740a7d133c99b4b438458a1098b2
thecaptain10 <purnimesh@gmail.com>
2020-06-09 01:11:04 +0530

    Middle of Linked List

---
+++ LeetCode/LeetCodeProblems/Medium/AngleClock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Medium
+{
+    //https://leetcode.com/problems/angle-between-hands-of-a-clock/
+    /*
+    Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand. 
+    */
+    //O()
+    public class AngleClock
+    {
+        public double ClockAngle(int hour, int minutes)
+        {
+
+            if (hour > 12 || minutes > 59)
+                return 0;
+
+            if (hour == 12 && minutes == 0)
+                return 0;
+
+            decimal hourPos = 0;
+            decimal minutePos = minutes;
+
+            if (hour == 12)
+                hourPos = 0;
+            else
+                hourPos = hour * 5;
+
+            //determine extra position change w.r.t minutes.
+            decimal extraPos = (((decimal)minutes / 60) * 5);
+
+            hourPos = hourPos + extraPos;
+
+            decimal diff = Math.Abs(minutePos - hourPos);
+
+            //multiply diff by 6 degree angle for each minute.
+            return ((double)Math.Min(diff * 6, 360 - (diff * 6)));
+
+
+        }
+    }
+}
